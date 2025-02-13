c1b893ff7137a15e1d2327628a4cdcdf618dfa15
thangckt <thangckt@gmail.com>
2025-02-13 18:50:54 +0000

    Update from https://github.com/thangckt/alff/commit/ba036257641999f6789ca9548f55386ad4c8c60c

--- _script/py_copy_config_schema.py
+++ _script/py_copy_config_schema.py
@@ -1,12 +1,10 @@
-import requests
-
-
 def read_schema_text(yaml_schema_file: str) -> str:
     """Read text from a file."""
     with open(yaml_schema_file, "r") as f:
         text = f.read()
     text = "\n```yaml\n" + text + "\n```\n"
     return text
+
 
 def append_schema_text(yaml_schema_file: str, text_file: str):
     """Append text to a file."""
@@ -18,18 +16,13 @@ def append_schema_text(yaml_schema_file: str, text_file: str):
 
 ##### ANCHOR: udpate the blog posts
 def main():
-    ### Post 1
     append_schema_text(yaml_schema_file="./alff/data/schema_arg_gendata.yaml", text_file="./_docs/schema/config_gendata.md")
-    ### Post 2
     append_schema_text(yaml_schema_file="./alff/al/schema_arg_active_learn.yaml", text_file="./_docs/schema/config_active_learning.md")
-    ### Post 3
     append_schema_text(yaml_schema_file="./alff/al/schema_arg_finetune.yaml", text_file="./_docs/schema/config_finetune.md")
-    ### Post 4
     append_schema_text(yaml_schema_file="./alff/phonon/schema_arg_phonon.yaml", text_file="./_docs/schema/config_phonon.md")
-    ### Post 5
     append_schema_text(yaml_schema_file="./alff/elastic/schema_arg_elastic.yaml", text_file="./_docs/schema/config_elastic.md")
-    ### Post 6
-    append_schema_text(yaml_schema_file="./alff/util/script/ase_script/schema_arg_ASE.yaml", text_file="./_docs/schema/config_ase.md")
+    append_schema_text(yaml_schema_file="./alff/util/script/ase_script/schema_arg_ase.yaml", text_file="./_docs/schema/config_ase.md")
+    append_schema_text(yaml_schema_file="./alff/util/schema_arg_machine.yaml", text_file="./_docs/schema/config_machine.md")
     return
 
 
