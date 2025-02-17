2e00553af24450f5df15dbd20efa2b129cebbe8d
thangckt <thangckt@gmail.com>
2025-02-17 16:29:29 +0000

    Update from https://github.com/thangckt/alff/commit/f606585d13485662c835e2141710ea8f8bec0bd3

--- _script/py_copy_config_schema.py
+++ _script/py_copy_config_schema.py
@@ -1,14 +1,22 @@
-def read_schema_text(yaml_schema_file: str) -> str:
-    """Read text from a file."""
+def append_schema_text(yaml_schema_file: str, text_file: str):
+    """Append yaml text to .md file."""
     with open(yaml_schema_file, "r") as f:
         text = f.read()
-    text = "\n```yaml\n" + text + "\n```\n"
-    return text
+    schema_text = "\n```yaml\n" + text + "\n```\n"
+    schema_text = "\n## Schema:\n" + schema_text
+    ### append yaml schema
+    with open(text_file, "a") as f:
+        f.write(schema_text)
+    return
 
 
-def append_schema_text(yaml_schema_file: str, text_file: str):
-    """Append text to a file."""
-    schema_text = read_schema_text(yaml_schema_file)
+def append_example_config(yaml_config_file: str, text_file: str):
+    """Append yaml text to .md file."""
+    with open(yaml_config_file, "r") as f:
+        text = f.read()
+    schema_text = "\n```yaml\n" + text + "\n```\n"
+    schema_text = "\n## Example configuration:\n" + schema_text
+    ### append yaml schema
     with open(text_file, "a") as f:
         f.write(schema_text)
     return
@@ -16,6 +24,7 @@ def append_schema_text(yaml_schema_file: str, text_file: str):
 
 ##### ANCHOR: udpate the blog posts
 def main():
+    ### Append schema to the .md files
     append_schema_text(yaml_schema_file="./alff/data/schema_arg_gendata.yaml", text_file="./_docs/schema/config_gendata.md")
     append_schema_text(yaml_schema_file="./alff/al/schema_arg_active_learn.yaml", text_file="./_docs/schema/config_active_learning.md")
     append_schema_text(yaml_schema_file="./alff/al/schema_arg_finetune.yaml", text_file="./_docs/schema/config_finetune.md")
@@ -23,6 +32,9 @@ def main():
     append_schema_text(yaml_schema_file="./alff/elastic/schema_arg_elastic.yaml", text_file="./_docs/schema/config_elastic.md")
     append_schema_text(yaml_schema_file="./alff/util/script/ase_script/schema_arg_ase.yaml", text_file="./_docs/schema/config_ase.md")
     append_schema_text(yaml_schema_file="./alff/util/schema_arg_machine.yaml", text_file="./_docs/schema/config_machine.md")
+    ### Append example configuration to the .md files
+    append_example_config(yaml_config_file="./alff/util/script/example_config/example_param_active_learn.yaml", text_file="./_docs/schema/config_active_learning.md")
+    append_example_config(yaml_config_file="./alff/util/script/example_config/example_param_ase.yaml", text_file="./_docs/schema/config_ase.md")
     return
 
 
